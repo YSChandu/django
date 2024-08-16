b92cd3435b43a0e047347fd4dc67cc6e08287496
Chandu <yarrapothusaichandu@gmail.com>
2024-08-16 22:18:15 +0530

    adding the bootstrap files

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request ,'home.html')
+    return render(request ,'index.html')
 
 def counter(request):
     words = request.POST['words']
