57ed65f2222b892a8f2500bbd96842ee02f3e17d
Chandu <yarrapothusaichandu@gmail.com>
2024-08-16 20:46:58 +0530

    stage 1

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request ,'home.html')
+
+
+def counter(request):
+    words = request.GET['words']
+    amount_of_words = len(words.split())
+    return render(request,'counter.html',{'amount':amount_of_words})
