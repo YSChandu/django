d722840fad3cd925e714cd501e402665007bf591
Chandu <yarrapothusaichandu@gmail.com>
2024-08-17 20:03:07 +0530

    admin panel and db manipulation

--- myapp/views.py
+++ myapp/views.py
@@ -3,36 +3,7 @@ from django.http import HttpResponse
 from .models import Feature
 # Create your views here.
 def index(request):
-    feature1=Feature()
-    feature1.id=0
-    feature1.name='Fast'
-    feature1.is_true=True
-    feature1.details='Our server is very quick'
-    
-    feature2=Feature()
-    feature2.id=1
-    feature2.name='Reliable'
-    feature2.is_true=True
-    feature2.details='Our server is very quick'
-    
-    feature3=Feature()
-    feature3.id=2
-    feature3.name='Easy To Use'
-    feature3.is_true=True
-    feature3.details='Our server is very quick'
-    
-    feature4=Feature()
-    feature4.id=3
-    feature4.is_true=False
-    feature4.name='Affordable'
-    feature4.details='Our server is very quick'
-    
-    feature5=Feature()
-    feature5.id=4
-    feature5.name='TrustWorthy'
-    feature5.is_true=True
-    feature5.details='Our server is filed with trust'
-    features =[feature1, feature2, feature3, feature4,feature5]
+    features=Feature.objects.all()
     return render(request ,'index.html',{'features':features})
 
 def counter(request):
